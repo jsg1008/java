4f8903718aaad3778df22b6408a5149d85a5442d
조성곤 <helnn@naver.com>
2025-06-04 22:40:11 +0900

    2025/06/04 at home

--- src/자료구조3장/Train_실습3_10_객체comparator구현.java
+++ src/자료구조3장/Train_실습3_10_객체comparator구현.java
@@ -1,6 +1,7 @@
 package 자료구조3장;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Student1{
 	private String sid;
@@ -27,40 +28,38 @@ class Student1{
 
 	@Override
 	public String toString() {
-		return sid + ", "+sname + ","+ dept;
+		return sid + ", "+sname + ", "+ dept;
 	}
-	
-	
-	
-	
 }
 
 public class Train_실습3_10_객체comparator구현 {
 	public static void main(String[] args) {
-		Student[] students = {
-			    new Student("S001", "영희", "Math"),
-			    new Student("S003", "민수", "Computer"),
-			    new Student("S002", "철수", "Physics"),
-			    new Student("S005", "지영", "Biology"),
-			    new Student("S004", "준호", "Chemistry")
+		Student1[] students = {
+			    new Student1("S001", "영희", "Math"),
+			    new Student1("S003", "민수", "Computer"),
+			    new Student1("S002", "철수", "Physics"),
+			    new Student1("S005", "지영", "Biology"),
+			    new Student1("S004", "준호", "Chemistry")
 			};
 		
 		System.out.println("=== 정렬 전 학생 목록 ===");
 		
 		show(students);
+		//정렬과 이진 탐색에 사용할 Comparator<Student> 객체를 람다식으로 구현. sid를 오름차순 비교. 
+		Comparator<Student1> comparator = (s1, s2) -> s1.getSid().compareTo(s2.getSid());
 		
-		Arrays.sort(students[], comparator);
+		Arrays.sort(students, comparator);
 		
-		Student[] targets = {
-			    new Student("S002", "철수", "Physics"),
-			    new Student("S006", "홍길동", "Law"),
-			    new Student("S004", "준호", "Chemistry")
+		Student1[] targets = {
+			    new Student1("S002", "철수", "Physics"),
+			    new Student1("S006", "홍길동", "Law"),
+			    new Student1("S004", "준호", "Chemistry")
 			};
 		
 		System.out.println("=== 이진 탐색 결과 ===");
 		
-		for (Student target : targets) {
-			int index = Arrays.binarySearch(students, target);
+		for (Student1 target : targets) {
+			int index = Arrays.binarySearch(students, target, comparator);//기준이 학번인 이유
 			if (index >=0) {
 				System.out.println("찾은 학생 : "+students[index]);
 			} else {
@@ -68,51 +67,10 @@ public class Train_실습3_10_객체comparator구현 {
 			}
 		}
 	}
-	private static void show(Student[]arr) {
-		for (Student ss : arr) {
+	
+	private static void show(Student1[]arr) {
+		for (Student1 ss : arr) {
 			System.out.println(ss);
 		}
 	}
-	
-
-/*
- * Comparator를 사용한 학생 정보 배열 정렬 및 이진 탐색 구현
- * 
- * 고정된 학생 배열을 Comparator를 사용해 학번(sid) 기준으로 정렬한 뒤,
- * 탐색 대상 학생 객체 배열에 대해 Arrays.binarySearch()를 사용하여 
- * 해당 학번을 가진 학생이 존재하는지 판단하는 프로그램을 작성하시오.
- * 
- * 구현 조건
-
-
-정렬과 이진 탐색에 사용할 Comparator<Student> 객체를 람다식 또는 
-
-기준: sid 오름차순
-
-
-위 배열을 Arrays.sort(배열, comparator)로 정렬한다.
-
-다음 탐색 대상 배열을 정의하고, 확장형 for문으로 각 항목에 대해 Arrays.binarySearch(배열, key, comparator)로 탐색한다.
-
-
-탐색 결과는 다음과 같이 출력한다:
-
-"찾은 학생: S002, 철수, Physics"
-
-"학번 S006인 학생은 존재하지 않습니다."
-
-=== 정렬 전 학생 목록 ===
-S001, 영희, Math
-S003, 민수, Computer
-S002, 철수, Physics
-S005, 지영, Biology
-S004, 준호, Chemistry
-
-=== 이진 탐색 결과 ===
-찾은 학생: S002, 철수, Physics
-학번 S006인 학생은 존재하지 않습니다.
-찾은 학생: S004, 준호, Chemistry
-
- * 
- */
 }
--- src/자료구조4장/Train_실습4_3_1정수선형큐_리스트.java
+++ src/자료구조4장/Train_실습4_3_1정수선형큐_리스트.java
@@ -4,23 +4,46 @@ package 자료구조4장;
  * 교재 148 페이지는 원형큐를 배열로 구현한 코드 실습 4번에 활용
  */
 
-import java.util.Scanner;
-
 /*
 * Queue of ArrayList
 */
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 class Queue4 {
 	private List<Integer> que;//원형큐로 구현하지 않는다 
 	private int capacity; // 큐의 크기
 	private int front; // 맨 처음 요소 커서
 	private int rear; // 맨 끝 요소 커서
-	private int num;
+	private int num; //front값과 rear값이 같을때 큐가 아예 빈건지, 아예 가득 찬건지를 구별하게 만드는 변수
+	
+	public Queue4(List<Integer> que, int capacity, int front, int rear, int num) {
+		this.que = que;
+		this.capacity = capacity;
+		this.front = front;
+		this.rear = rear;
+		this.num = num;
+	}
+	
+	public List<Integer> getQue() {
+		return que;
+	}
 
-//--- 실행시 예외: 큐가 비어있음 ---//
+	public int getFront() {
+		return front;
+	}
+
+	public int getRear() {
+		return rear;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	//--- 실행시 예외: 큐가 비어있음 ---//
 	public class EmptyQueueException extends RuntimeException {
 		public EmptyQueueException(String message) {
 			super(message);
@@ -36,37 +59,63 @@ class Queue4 {
 
 //--- 생성자(constructor) ---//
 	public Queue4(int maxlen) {
-		num = front = rear = 0;
+		num = front = rear = 0; 
 		capacity = maxlen;
-		try {
-			que = new List<>[capacity];
-		} catch (OutofMemoryError e) {
-			capacity = 0;
-		}
+		que = new ArrayList<>(); 
 	}
 
 //--- 큐에 데이터를 인큐 ---//
 	public int enque(int x) throws OverflowQueueException {
-
+		if (num >= capacity) {
+			throw new OverflowQueueException("가득 찼습니다.");
+		}
+		que.add(x);
+		rear++;
+		num++;
+		if (rear==capacity) {
+			rear=0;
+		}
+		return x;
 	}
 
 //--- 큐에서 데이터를 디큐 ---//
 	public int deque() throws EmptyQueueException {
-
+		if (num<= 0) {
+			throw new EmptyQueueException("비어 있습니다.");
+		}
+		int x = que.remove(front);
+		if (front == capacity -1) {
+			front = 0;
+		} else {
+			front++; //1개를 뺏으니 남아있는 front자리는 하나 더 뒤임
+		}
+		num--;//1개를 뺐으니 1개 줄어듬
+		return x; 
 	}
 
 //--- 큐에서 데이터를 피크(프런트 데이터를 들여다봄) ---//
 	public int peek() throws EmptyQueueException {
-
+		if(isEmpty()) {
+			throw new EmptyQueueException("비어 있습니다.");
+		}
+		return que.get(front);//List이므로 que[front]를 쓰지 않음.
 	}
 
 //--- 큐를 비움 ---//
 	public void clear() {
-		front = rear = 0;
+		num = front = rear = 0;
 	}
 
 //--- 큐에서 x를 검색하여 인덱스(찾지 못하면 –1)를 반환 ---//
 	public int indexOf(int x) {
+//		return que.indexOf(x);
+		for (int i = 0; i < num; i++) {
+			int idx = (i + front) % capacity;
+			if (que.get(idx)==x) {
+				return idx;
+			}
+		}
+		return -1;
 
 	}
 
@@ -77,25 +126,32 @@ class Queue4 {
 
 //--- 큐에 쌓여 있는 데이터 개수를 반환 ---//
 	public int size() {
-
+		return num;
 	}
 
 //--- 큐가 비어있는가? ---//
 	public boolean isEmpty() {
-	
+		return num <= 0;
 	}
 
 //--- 큐가 가득 찼는가? ---//
 	public boolean isFull() {
-
+		return num >= capacity;
 	}
 
 //--- 큐 안의 모든 데이터를 프런트 → 리어 순으로 출력 ---//
 	public void dump() {
-
+		if (num <=0) {
+			System.out.println("큐가 비어 있습니다.");
+		} else {
+			for (int i=0; i < num ; i++) {
+				System.out.println(que.get((i+front)%capacity) + " ");
+				System.out.println();
+			}
+		}
 	}
 }
-public class train_실습4_3_1정수선형큐_리스트 {
+public class Train_실습4_3_1정수선형큐_리스트 {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
 		Queue4 oq = new Queue4(64); // 최대 64개를 인큐할 수 있는 큐
